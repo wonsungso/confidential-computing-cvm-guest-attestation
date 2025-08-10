7e7a9549380bfda8f0d7371f5f8496d033cd5139
Wonsung So <wonsungso@microsoft.com>
2025-08-10 15:50:22 +0900

    Commit

--- cvm-attestation-sample-app/ClientLibBuildAndInstall.sh
+++ cvm-attestation-sample-app/ClientLibBuildAndInstall.sh
@@ -2,4 +2,5 @@
 
 sudo ../client-library/src/Attestation/pre-requisites.sh
 sudo ../client-library/src/Attestation/build.sh
-sudo dpkg -i ../client-library/src/Attestation/_build/x86_64/packages/attestationlibrary/deb/azguestattestation1_1.0.5_amd64.deb
+sudo wget https://packages.microsoft.com/repos/azurecore/pool/main/a/azguestattestation1/azguestattestation1_1.0.5_amd64.deb
+sudo dpkg -i ./azguestattestation1_1.0.5_amd64.deb
