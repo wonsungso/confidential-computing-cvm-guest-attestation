d2013b6cf78047a96816f9a72f955ccbcc29036d
Wonsung So <wonsungso@microsoft.com>
2025-10-28 10:38:24 +0900

    fix the script location

--- cvm-attestation-sample-app/ClientLibBuildAndInstall.sh
+++ cvm-attestation-sample-app/ClientLibBuildAndInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo ../scripts/pre-requisites.sh
-sudo ../scripts/build.sh
+sudo ./scripts/pre-requisites.sh
+sudo ./scripts/build.sh
 sudo wget https://packages.microsoft.com/repos/azurecore/pool/main/a/azguestattestation1/azguestattestation1_1.0.5_amd64.deb
 sudo dpkg -i ./azguestattestation1_1.0.5_amd64.deb
